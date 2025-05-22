fd9fef8e75432c4754d6ff9edf0b2d353926dd5f
Ana Júlia da Cunha <101332630+cunhanai@users.noreply.github.com>
2025-05-21 21:25:23 -0300

    quicksort - teorema mestre incompleto

--- src/Ordenador.java
+++ src/Ordenador.java
@@ -1,14 +1,13 @@
 public class Ordenador {
     // bubbleSort
 
-    // pior caso: 1 + n * (2 + 5n)
-    //          = 1 + 2n + 5n^2
-    //          = 5n^2 + 2n + 1
+    // pior caso: 2 + n * (2 + 5n)
+    //          = 2 + 2n + 5n^2
+    //          = 5n^2 + 2n + 2
     //          = O(n^2)
-    //          // caso o vetor esteja ordenado de forma inversa
-    // melhor caso: 1 + n * (2 + 1n)
-    //          = 1 + 2n + n^2
-    //          = n^2 + 2n + 1
+    // melhor caso: 2 + n * (2 + 1n)
+    //          = 2 + 2n + n^2
+    //          = n^2 + 2n + 2
     //          = Ω(n^2)
     //          caso o vetor já esteja ordenado
 
@@ -32,56 +31,70 @@ public class Ordenador {
             }
         }
 
-        return info;
+    return info;                                    // 1
     }
 
     // quickSort
 
+    // pior caso: (2) + ()
+    // melhor caso: (2) + (2)
+    //             4
+    //             caso o vetor esteja vazio ou tenha apenas 1 elemento
     public int[] quickSort(int[] info){ 
-        int n = info.length - 1;
-        return quickSort(info, 0, n);
+        int n = info.length - 1;                    // 1
+        return quickSort(info, 0, n);         // 1
     }
 
+    // pior caso: (2 + 2T(n)) + (2n^2 + 6n + 9)
+    //           
+    // melhor caso caso: 2
     private int[] quickSort(int[] info, int start, int end) {
-        if (start < end) {
-            int index = partition(info, start, end);
-            info = quickSort(info, start, index - 1);
-            info = quickSort(info, index + 1, end);
+        if (start < end) {                                      // 1
+            int index = partition(info, start, end);                // 1   
+            info = quickSort(info, start, index - 1);               // T(n)
+            info = quickSort(info, index + 1, end);                 // T(n)
         }
 
-        return info;
+        return info;                                            // 1
     }
 
+    // pior caso: 9 + n * (n + n + 6)
+    //          = 9 + 2n^2 + 6n
+    //          = 2n^2 + 6n + 9
+    // melhor caso: 9 + n * 8
+    //          = 9 + 8n
+    //          = 8n + 9
     private int partition(int[] info, int start, int end) {
-        int a = start;
-        int b = end +1;
-        int pivot = info[start];
+        int a = start;                                      // 1
+        int b = end +1;                                     // 1
+        int pivot = info[start];                            // 1
 
-        while (true) {
+        while (true) {                                      // n
             do {
-                a++;
-            } while (a <= end && info[a] < pivot);
+                a++;                                                // 1
+            } while (a <= end && info[a] < pivot);              // n
 
             do {
-                b--;
-            } while (b > start && info[b] > pivot);
+                b--;                                                // 1
+            } while (b > start && info[b] > pivot);             // n
 
-            if (a >= b) {
+            if (a >= b) {                                       // 1
                 break;
             }
 
-            info = swap(info, a, b);
+            info = swap(info, a, b);                            // 1 + 4
         }
 
-        info = swap(info, b, start);
-        return b;
+        info = swap(info, b, start);                        // 1 + 4
+        return b;                                           // 1
     }   
 
+    // melhor e pior caso = 4
     private int[] swap(int[] info, int a, int b) {
-        int temp = info[a];
-        info[a] = info[b];
-        info[b] = temp;
-        return info;
+        int temp = info[a];                         // 1
+        info[a] = info[b];                          // 1  
+        info[b] = temp;                             // 1
+        return info;                                // 1
     }
 
     // mergeSort
